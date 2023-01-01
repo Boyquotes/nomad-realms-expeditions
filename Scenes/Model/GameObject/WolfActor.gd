22de8fe9ba1d113a9cb079ace244629fe92d7245
JaryJay <jay.jrjren@gmail.com>
2023-01-01 17:20:15 -0500

    Add wolf 3D model, fix wolf highlighting

--- Scenes/Model/GameObject/WolfActor.gd
+++ Scenes/Model/GameObject/WolfActor.gd
@@ -1,2 +1,15 @@
 extends CardPlayer
 class_name WolfActor
+
+@export var highlight_flash: ShaderMaterial
+@onready var mesh: MeshInstance3D = $wolf/Cube
+
+func _ready():
+	rotate_y(randf_range(0, TAU))
+
+func set_highlighted(h: bool) -> void:
+	super.set_highlighted(h)
+	if h:
+		mesh.material_overlay = highlight_flash
+	else:
+		mesh.material_overlay = null
