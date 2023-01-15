7f85c7a1d27883385b1c30d64843b1e73fa8d32e
Jay <jay.jrjren@gmail.com>
2023-01-14 22:28:04 -0500

    Create CardDashboard copy() method

--- Scenes/Model/Card/CardDashboard.gd
+++ Scenes/Model/Card/CardDashboard.gd
@@ -4,3 +4,10 @@ class_name CardDashboard
 var deck: Array[GameCard] = []
 var hand: Array[GameCard] = []
 var discard: Array[GameCard] = []
+
+func copy() -> CardDashboard:
+	var copy = CardDashboard.new()
+	copy.deck.append_array(deck)
+	copy.hand.append_array(hand)
+	copy.discard.append_array(discard)
+	return copy
