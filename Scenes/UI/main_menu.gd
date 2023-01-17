14ee5c53cf0258659676c9b73e216c5bc8f852e5
Jay <jay.jrjren@gmail.com>
2023-01-16 22:16:35 -0500

    Make main menu transition to game

--- Scenes/UI/main_menu.gd
+++ Scenes/UI/main_menu.gd
@@ -1,8 +1,12 @@
 extends Control
 
+@export var game_scene: PackedScene
+
+@onready var click_to_start_label: Label = $ClickToStartLabel
+
 func _unhandled_input(event):
 	if event is InputEventMouseButton:
 		var mouse_button_event: InputEventMouseButton = event
 		if mouse_button_event.button_index == MOUSE_BUTTON_LEFT:
-			# TODO: transition to game
-			pass
+			queue_free()
+			get_tree().root.add_child(game_scene.instantiate())
