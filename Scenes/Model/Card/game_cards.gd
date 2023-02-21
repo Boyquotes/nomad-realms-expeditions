b56272758506178849d9866772fd95e94deff18c
JaryJay <47123100+JaryJay@users.noreply.github.com>
2023-02-21 10:11:05 -0500

    Fix bug where game would instantiate twice

--- Scenes/Model/Card/game_cards.gd
+++ Scenes/Model/Card/game_cards.gd
@@ -10,10 +10,16 @@ var TELEPORT: = GameCard.new("Teleport", 1, \
 	CardEffect.new(0b0001, range_predicate(4), TeleportExpression.new()))
 var REGENESIS: = GameCard.new("Regenesis", 10, \
 	"Shuffle your discard into your deck", BASIC, \
-	CardEffect.new(0b0000, null, RegenesisExpression.new()))
+	CardEffect.new(0b0000, none(), RegenesisExpression.new()))
 var SLASH: = GameCard.new("Slash", 1, \
 	"Deal 4 to a target within range 2", BASIC, \
 	CardEffect.new(0b1110, range_predicate(2), MeleeDamageExpression.new(4)))
+
+# Predicates
+
+func none() -> Callable:
+	return func(cp, ct):
+		return true
 
 func range_predicate(range: int) -> Callable:
 	return func(cp, ct):
--- Scenes/UI/main_menu.gd
+++ Scenes/UI/main_menu.gd
@@ -7,6 +7,7 @@ extends Control
 func _unhandled_input(event):
 	if event is InputEventMouseButton:
 		var mouse_button_event: InputEventMouseButton = event
-		if mouse_button_event.button_index == MOUSE_BUTTON_LEFT:
-			queue_free()
+		if mouse_button_event.button_index == MOUSE_BUTTON_LEFT && !mouse_button_event.pressed:
 			get_tree().root.add_child(game_scene.instantiate())
+			queue_free()
+			visible = false
