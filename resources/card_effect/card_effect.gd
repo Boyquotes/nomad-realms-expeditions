82170d96181ddc407036a278f2ff1dd1565c2481
Jay <jay.jrjren@gmail.com>
2023-04-07 22:26:58 -0400

    Create CardEffect.calculate_playability(...) function

--- resources/card_effect/card_effect.gd
+++ resources/card_effect/card_effect.gd
@@ -3,3 +3,10 @@ class_name CardEffect
 
 func _handle(actor: Actor, target: Actor) -> void:
 	pass
+
+func suggest_targets(surroundings: Array[Array], player: Actor) -> Array[Actor]:
+	return []
+
+func calculate_playability(surroundings: Array[Array], player: Actor) -> float:
+	assert(false, "ERROR: calculate_playability() function not overriden.")
+	return 0
--- resources/card_effect/damage_effect.gd
+++ resources/card_effect/damage_effect.gd
@@ -5,3 +5,27 @@ class_name DamageEffect
 
 func _handle(actor: Actor, target: Actor) -> void:
 	target.health_component.health -= damage
+
+# Override
+func calculate_playability(surroundings: Array[Array], player: Actor) -> float:
+	var card_player_comp: = player.card_player_component
+	var health_comp: = player.health_component
+	var v: int = 0
+	
+	if surroundings:
+		for i in range(surroundings.size()):
+			for j in range(surroundings[i].size()):
+				var a: Actor = surroundings[i][j]
+				if not a or not a.health_component:
+					continue
+				if player.is_an_enemy_to(a):
+					v -= a.health_component.health
+				else:
+					v += a.health_component.health
+	
+	if health_comp:
+		v += health_comp.health / health_comp.starting_health
+	if card_player_comp:
+		v += card_player_comp.hand.size()
+	
+	return v
